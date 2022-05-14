842187c1a3486c4192230d7a44ea6d6f1a3496f3
rishiamritbootisewasansthan <105221880+rishiamritbootisewasansthan@users.noreply.github.com>
2022-05-14 20:10:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      h1{
+      text-align:center;
+      }
+      body{
+      background-imge: url(i.jpg);
+        background-size:cover;
+      }
+    </style>
+  </head>
+<body>
+
+<h1>Guru ji Govind - Ayurvedacharya & Spiritual Yoga Guru</h1>
+
+</body>
+</html>
 
